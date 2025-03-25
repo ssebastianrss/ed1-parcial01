c08be1ee9cd7e6093e597b49cdbc72d15bdb45fc
Sebastian <sruanos@correo.url.edu.gt>
2025-03-25 07:35:22 -0600

    E01 Flatten Binary Tree

--- src/main/java/ed/lab/E01FlattenBT.java
+++ src/main/java/ed/lab/E01FlattenBT.java
@@ -3,7 +3,25 @@ package ed.lab;
 public class E01FlattenBT {
 
     public void flatten(TreeNode<Integer> root) {
+        if (root == null) {
+            return;
+        }
 
+        if (root.left != null) {
+            flatten(root.left);
+
+            TreeNode<Integer> aux = root.right;
+            root.right = root.left;
+            root.left = null;
+
+            TreeNode<Integer> current = root.right;
+            while (current.right != null) {
+                current = current.right;
+            }
+
+            current.right = aux;
+        }
+
+        flatten(root.right);
     }
-
 }
